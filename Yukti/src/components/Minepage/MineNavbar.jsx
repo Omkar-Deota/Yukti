a970e2afb9c20ee440d0dd550e12e50539857d58
Omkar-Deota <omkarinshu2003@gmail.com>
2024-03-16 12:45:32 +0530

    Menu added

--- Yukti/src/components/Minepage/MineNavbar.jsx
+++ Yukti/src/components/Minepage/MineNavbar.jsx
@@ -1,227 +1,3 @@
-// import * as React from 'react';
-// import { styled, alpha } from '@mui/material/styles';
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import InputBase from '@mui/material/InputBase';
-// import Badge from '@mui/material/Badge';
-// import MenuItem from '@mui/material/MenuItem';
-// import Menu from '@mui/material/Menu';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import SearchIcon from '@mui/icons-material/Search';
-// import AccountCircle from '@mui/icons-material/AccountCircle';
-// import MailIcon from '@mui/icons-material/Mail';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
-// import MoreIcon from '@mui/icons-material/MoreVert';
-
-// const Search = styled('div')(({ theme }) => ({
-//   position: 'relative',
-//   borderRadius: theme.shape.borderRadius,
-//   backgroundColor: alpha(theme.palette.common.white, 0.15),
-//   '&:hover': {
-//     backgroundColor: alpha(theme.palette.common.white, 0.25),
-//   },
-//   marginRight: theme.spacing(2),
-//   marginLeft: 0,
-//   width: '100%',
-//   [theme.breakpoints.up('sm')]: {
-//     marginLeft: theme.spacing(3),
-//     width: 'auto',
-//   },
-// }));
-
-// const SearchIconWrapper = styled('div')(({ theme }) => ({
-//   padding: theme.spacing(0, 2),
-//   height: '100%',
-//   position: 'absolute',
-//   pointerEvents: 'none',
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-// }));
-
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//   color: 'inherit',
-//   '& .MuiInputBase-input': {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//     transition: theme.transitions.create('width'),
-//     width: '100%',
-//     [theme.breakpoints.up('md')]: {
-//       width: '20ch',
-//     },
-//   },
-// }));
-
-// export default function MineNavbar() {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-//   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-//   const isMenuOpen = Boolean(anchorEl);
-//   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//   const handleProfileMenuOpen = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleMobileMenuClose = () => {
-//     setMobileMoreAnchorEl(null);
-//   };
-
-//   const handleMenuClose = () => {
-//     setAnchorEl(null);
-//     handleMobileMenuClose();
-//   };
-
-//   const handleMobileMenuOpen = (event) => {
-//     setMobileMoreAnchorEl(event.currentTarget);
-//   };
-
-//   const menuId = 'primary-search-account-menu';
-//   const renderMenu = (
-//     <Menu
-//       anchorEl={anchorEl}
-//       anchorOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       id={menuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       open={isMenuOpen}
-//       onClose={handleMenuClose}
-//     >
-//       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-//       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-//     </Menu>
-//   );
-
-//   const mobileMenuId = 'primary-search-account-menu-mobile';
-//   const renderMobileMenu = (
-//     <Menu
-//       anchorEl={mobileMoreAnchorEl}
-//       anchorOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       id={mobileMenuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       open={isMobileMenuOpen}
-//       onClose={handleMobileMenuClose}
-//     >
-//       <MenuItem>
-//         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-//           <Badge badgeContent={4} color="error">
-//             <MailIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Messages</p>
-//       </MenuItem>
-//       <MenuItem>
-//         <IconButton
-//           size="large"
-//           aria-label="show 17 new notifications"
-//           color="inherit"
-//         >
-//           <Badge badgeContent={17} color="error">
-//             <NotificationsIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Notifications</p>
-//       </MenuItem>
-//       <MenuItem onClick={handleProfileMenuOpen}>
-//         <IconButton
-//           size="large"
-//           aria-label="account of current user"
-//           aria-controls="primary-search-account-menu"
-//           aria-haspopup="true"
-//           color="inherit"
-//         >
-//           <AccountCircle />
-//         </IconButton>
-//         <p>Profile</p>
-//       </MenuItem>
-//     </Menu>
-//   );
-
-//   return (
-//     <Box sx={{ flexGrow: 1 }}>
-//       <AppBar position="static" sx={{backgroundColor:'black'}}>
-//         <Toolbar>
-        
-//           <Typography
-//             variant="h6"
-//             noWrap
-//             component="div"
-//             sx={{ display: { xs: 'none', sm: 'block' } }}
-//           >
-//             Ember
-//           </Typography>
-//           <Search>
-//             <SearchIconWrapper>
-//               <SearchIcon />
-//             </SearchIconWrapper>
-//             <StyledInputBase
-//               placeholder="Search…"
-//               inputProps={{ 'aria-label': 'search' }}
-//             />
-//           </Search>
-//           <Box sx={{ flexGrow: 1 }} />
-//           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-//             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-//               <Badge badgeContent={4} color="error">
-//                 <MailIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               size="large"
-//               aria-label="show 17 new notifications"
-//               color="inherit"
-//             >
-//               <Badge badgeContent={17} color="error">
-//                 <NotificationsIcon />
-//               </Badge>
-//             </IconButton>
-            
-//             <IconButton
-//             size="large"
-//             edge="end"
-//             color="inherit"
-//             aria-label="open drawer"
-//             sx={{ mr: 2 }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           </Box>
-//           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="show more"
-//               aria-controls={mobileMenuId}
-//               aria-haspopup="true"
-//               onClick={handleMobileMenuOpen}
-//               color="inherit"
-//             >
-//               <MoreIcon />
-//             </IconButton>
-//           </Box>
-//         </Toolbar>
-//       </AppBar>
-//       {renderMobileMenu}
-//       {renderMenu}
-//     </Box>
-//   );
-// }
 import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
@@ -239,8 +15,6 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import Drawer from '@mui/material/Drawer';
-import { Divider } from '@mui/material';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -281,33 +55,18 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }));
-const StyledDrawer = styled(Drawer)(({ theme }) => ({
-  '& .MuiDrawer-paper': {
-    backgroundColor: theme.palette.info.main, // Change background color
-    color: theme.palette.text.primary, // Change font color
-    fontSize:'1.5rem'
-  },
-}))
-
 
 export default function MineNavbar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const [menuAnchorEl, setMenuAnchorEl] = React.useState(null);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMenuIconOpen = Boolean(menuAnchorEl);
 
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
-  };
-
-  const handleDrawerOpen = () => {
-    setIsDrawerOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setIsDrawerOpen(false);
   };
 
   const handleMobileMenuClose = () => {
@@ -321,6 +80,14 @@ export default function MineNavbar() {
 
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuIconClick = (event) => {
+    setMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuIconClose = () => {
+    setMenuAnchorEl(null);
   };
 
   const menuId = 'primary-search-account-menu';
@@ -397,22 +164,45 @@ export default function MineNavbar() {
     </Menu>
   );
 
+  const renderMenuIcon = (
+    <Menu 
+      anchorEl={menuAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id="menu-icon"
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuIconOpen}
+      onClose={handleMenuIconClose}
+    >
+      <MenuItem onClick={handleMenuIconClose}>Make Request</MenuItem>
+      <MenuItem onClick={handleMenuIconClose}>Reschedule</MenuItem>
+      <MenuItem onClick={handleMenuIconClose}>Completed Orders</MenuItem>
+      <MenuItem onClick={handleMenuIconClose}>Revenue</MenuItem>
+    </Menu>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{backgroundColor:'black'}}>
+      <AppBar position="static" sx={{ backgroundColor: 'black' }}>
         <Toolbar>
-        <IconButton
-              size="large"
-              edge="start"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          
+          <IconButton
+            size="large"
+            edge="start"
+            aria-label="account of current user"
+            aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+
           <Typography
             variant="h6"
             noWrap
@@ -432,7 +222,11 @@ export default function MineNavbar() {
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
               <Badge badgeContent={4} color="error">
                 <MailIcon />
               </Badge>
@@ -458,40 +252,22 @@ export default function MineNavbar() {
             >
               <MoreIcon />
             </IconButton>
-           
           </Box>
           <IconButton
             size="large"
             edge="end"
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
             sx={{ mr: 2 }}
+            onClick={handleMenuIconClick} // Adding onClick event for menu icon
           >
             <MenuIcon />
           </IconButton>
-          
         </Toolbar>
-
       </AppBar>
       {renderMobileMenu}
       {renderMenu}
-      <StyledDrawer
-        anchor="right"
-  
-        open={isDrawerOpen}
-        onClose={handleDrawerClose}
-        
-      >
-        <MenuItem onClick={handleDrawerClose} sx={{fontSize:'1.5rem'}}>Request Rake</MenuItem>
-        <Divider/>
-        <MenuItem onClick={handleDrawerClose} sx={{fontSize:'1.5rem'}}>Update Rake</MenuItem>
-        <Divider/>
-        <MenuItem onClick={handleDrawerClose} sx={{fontSize:'1.5rem'}}>Orders Received</MenuItem>
-        <Divider/>
-        <MenuItem onClick={handleDrawerClose} sx={{fontSize:'1.5rem'}}>Revenue</MenuItem>
-      </StyledDrawer>
-      
+      {renderMenuIcon} {/* Adding the menu icon dropdown */}
     </Box>
   );
 }
